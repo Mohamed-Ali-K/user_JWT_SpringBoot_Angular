69e0d547e8b973c7fac488b4856f2969f087d9dc
KENIS Mohamed Ali <kenis.mohamed.ali@gmail.com>
2023-01-06 11:01:04 +0100

    fix the url

--- src/main/java/com/kenis/supportportal/resource/UserResource.java
+++ src/main/java/com/kenis/supportportal/resource/UserResource.java
@@ -1,13 +1,13 @@
 package com.kenis.supportportal.resource;
 
 
-import com.kenis.supportportal.domain.User;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/user")
 public class UserResource {
 
     @GetMapping("/home")
