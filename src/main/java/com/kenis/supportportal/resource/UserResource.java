5bbb85f726bc7794b0decd9ab4417912fbb0d8f3
KENIS Mohamed Ali <kenis.mohamed.ali@gmail.com>
2023-01-07 10:09:12 +0100

    adding controller method in UserResource.java that handles a POST request to the /register endpoint.

--- src/main/java/com/kenis/supportportal/resource/UserResource.java
+++ src/main/java/com/kenis/supportportal/resource/UserResource.java
@@ -3,19 +3,30 @@ package com.kenis.supportportal.resource;
 
 
 
+import com.kenis.supportportal.domain.User;
+import com.kenis.supportportal.exception.domain.EmailExistException;
 import com.kenis.supportportal.exception.domain.ExceptionHandling;
+import com.kenis.supportportal.exception.domain.UserNameExistException;
 import com.kenis.supportportal.exception.domain.UserNotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kenis.supportportal.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = {"/","/user"})
 public class UserResource extends ExceptionHandling {
+    private UserService userService;
 
-    @GetMapping("/home")
-    public String showUser() throws UserNotFoundException{
-        //return "Application Work";
-        throw new UserNotFoundException("User Not Found Exception");
+    @Autowired
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) throws UserNotFoundException, EmailExistException, UserNameExistException {
+        User newUser = userService.register(user.getFirstName(), user.getLastName(), user.getEmail(), user.getUserName());
+        return new ResponseEntity<>(newUser, HttpStatus.OK);
     }
 }
