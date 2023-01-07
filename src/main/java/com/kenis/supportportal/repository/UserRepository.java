25db24819d8d56090491939006a106e56b3f8dc4
KENIS Mohamed Ali <kenis.mohamed.ali@gmail.com>
2023-01-07 10:07:02 +0100

    add the ability to Finds the user with the specified user ID.

--- src/main/java/com/kenis/supportportal/repository/UserRepository.java
+++ src/main/java/com/kenis/supportportal/repository/UserRepository.java
@@ -10,4 +10,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     User findUserByUserName(String username);
     User findUserByEmail(String email);
+    User findUserByUserId(String userId);
 }
