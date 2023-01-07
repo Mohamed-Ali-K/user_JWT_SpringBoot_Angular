d202e19ba08f3f402d0f8de96353178575f2fac7
KENIS Mohamed Ali <kenis.mohamed.ali@gmail.com>
2023-01-07 11:48:53 +0100

    minor update

--- src/main/java/com/kenis/supportportal/constant/SecurityConstant.java
+++ src/main/java/com/kenis/supportportal/constant/SecurityConstant.java
@@ -11,9 +11,5 @@ public class SecurityConstant {
     public static final String FORBIDDEN_MESSAGE = "You need to log in to access this page";
     public static final String ACCESS_DENIED_MESSAGE = "you do not have permission to access this page";
     public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
-    public static final String[] PUBLIC_URLS = {
-            "/user/login",
-            "user/register",
-            "/user/ressetpassword/**",
-            "/user/image/**"};
+    public static final String[] PUBLIC_URLS = {"**"};
 }
