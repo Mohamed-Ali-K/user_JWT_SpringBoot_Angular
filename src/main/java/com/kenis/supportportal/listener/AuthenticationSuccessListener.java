8c8552dffe9b861fdad2769c1ac110a29f13ebc4
KENIS Mohamed Ali <kenis.mohamed.ali@gmail.com>
2023-01-08 12:47:13 +0100

    fixing bug where instead of getting UserPrincipal we get User

--- src/main/java/com/kenis/supportportal/listener/AuthenticationSuccessListener.java
+++ src/main/java/com/kenis/supportportal/listener/AuthenticationSuccessListener.java
@@ -1,13 +1,13 @@
 package com.kenis.supportportal.listener;
 
 import com.kenis.supportportal.domain.User;
+import com.kenis.supportportal.domain.UserPrincipal;
 import com.kenis.supportportal.service.LoginAttemptService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.ExecutionException;
 
 /**
  * <h3>Listener that processes {@link AuthenticationSuccessEvent} events.</h3>
@@ -55,8 +55,8 @@ public class AuthenticationSuccessListener {
     @EventListener
     public void onAuthenticationSuccess(AuthenticationSuccessEvent event) {
         Object principal = event.getAuthentication().getPrincipal();
-        if (principal instanceof User) {
-            User user = (User) event.getAuthentication().getPrincipal();
+        if (principal instanceof UserPrincipal) {
+            UserPrincipal user = (UserPrincipal) event.getAuthentication().getPrincipal();
             loginAttemptService.evictUserFromLoginAttemptCache(user.getUsername());
         }
     }
