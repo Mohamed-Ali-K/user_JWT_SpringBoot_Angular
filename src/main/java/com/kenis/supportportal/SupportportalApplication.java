4f1299105ec1248aaf585937ac27891c567d163e
KENIS Mohamed Ali <kenis.mohamed.ali@gmail.com>
2023-01-09 09:55:19 +0100

    Updated UserService interface with new methods for adding, updating, deleting, and resetting passwords for users. Also added method for sending reset password email.

--- src/main/java/com/kenis/supportportal/SupportportalApplication.java
+++ src/main/java/com/kenis/supportportal/SupportportalApplication.java
@@ -5,11 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.io.File;
+
+import static com.kenis.supportportal.constant.FileConstant.USER_FOLDER;
+
 @SpringBootApplication
 public class SupportportalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupportportalApplication.class, args);
+		new File(USER_FOLDER).mkdirs();
 	}
 
 	@Bean
--- src/main/java/com/kenis/supportportal/service/UserService.java
+++ src/main/java/com/kenis/supportportal/service/UserService.java
@@ -2,15 +2,19 @@ package com.kenis.supportportal.service;
 
 import com.kenis.supportportal.domain.User;
 import com.kenis.supportportal.exception.domain.EmailExistException;
-import com.kenis.supportportal.exception.domain.UserNameExistException;
+import com.kenis.supportportal.exception.domain.EmailNotFoundException;
+import com.kenis.supportportal.exception.domain.UsernameExistException;
 import com.kenis.supportportal.exception.domain.UserNotFoundException;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
+import java.io.IOException;
 import java.util.List;
 
 /**
  * This is an interface for a service that manages users.
  * It provides methods for registering users, retrieving a list of users,
+ * adding new users, updating existing users, deleting users, resetting passwords,
  * and finding a user by username or email.
  *
  * @author Mohamed Ali Kenis
@@ -22,11 +26,15 @@ public interface UserService {
      *
      * @param firstName the user's first name
      * @param lastName the user's last name
-     * @param userName the user's username
+     * @param username the user's username
      * @param email the user's email address
      * @return the registered user
+     * @throws UserNotFoundException if the user cannot be found
+     * @throws EmailExistException if the email already exists
+     * @throws UsernameExistException if the username already exists
+     * @throws MessagingException if there is an error sending an email
      */
-    User register(String firstName, String lastName, String userName, String email) throws UserNotFoundException, EmailExistException, UserNameExistException, MessagingException;
+    User register(String firstName, String lastName, String username, String email) throws UserNotFoundException, EmailExistException, UsernameExistException, MessagingException;
 
 
     /**
@@ -40,10 +48,10 @@ public interface UserService {
     /**
      * Finds a user by their username.
      *
-     * @param userName the user's username
+     * @param username the user's username
      * @return the user, or null if no such user exists
      */
-    User findUserByUserName(String userName);
+    User findUserByUsername(String username);
 
 
     /**
@@ -53,6 +61,76 @@ public interface UserService {
      * @return the user, or null if no such user exists
      */
     User findUserByEmail(String email);
+
+    /**
+     * Adds a new user with the given information.
+     *
+     * @param firstName the user's first name
+     * @param lastName the user's last name
+     * @param username the user's username
+     * @param email the user's email address
+     * @param role the user's role
+     * @param isNotLocked whether the user is locked or not
+     * @param isActive whether the user is active or not
+     * @param profileImage the user's profile image
+     * @return the added user
+     */
+    User addNewUser(
+            String firstName,
+            String lastName,
+            String username,
+            String email,
+            String role,
+            boolean isNotLocked,
+            boolean isActive, MultipartFile profileImage) throws UserNotFoundException, EmailExistException, UsernameExistException, IOException;
+
+/**
+ * Updates the information for the user with the given username.
+ *
+ * @param currentUsername the current username of the user
+ * @param newFirstName the new first name for the user
+ * @param newLastName the new last name for the user
+ * @param newUsername the new username for the user
+ * @param newEmail the new email for the user
+ * @param role the new role for the user
+ * @param isNotLocked whether the user should be locked or not
+ * @param isActive whether the user should be active or not
+ * @param profileImage the new profile image for the user
+ * @return the updated user
+ */
+    User updateUser(
+            String currentUsername,
+            String newFirstName,
+            String newLastName,
+            String newUsername,
+            String newEmail,
+            String role,
+            boolean isNotLocked,
+            boolean isActive,
+            MultipartFile profileImage) throws UserNotFoundException, EmailExistException, UsernameExistException, IOException;
+
+    /**
+     * Deletes the user with the given ID.
+     *
+     * @param id the ID of the user to delete
+     */
+    void deleteUser(Long id);
+
+    /**
+     * Resets the password for the user with the given email address.
+     *
+     * @param email the email address of the user
+     */
+    void resetPassword(String email) throws MessagingException, EmailNotFoundException;
+
+    /**
+     * Updates the profile image for the user with the given username.
+     *
+     * @param username the username of the user
+     * @param newProfileImage the new profile image for the user
+     * @return the updated user
+     */
+    User updateProfileImage(String username, MultipartFile newProfileImage) throws UserNotFoundException, EmailExistException, UsernameExistException, IOException;
 }
 
 
