5efc752a023dded6651e19fa41b20b7a9a2e2a70
KENIS Mohamed Ali <kenis.mohamed.ali@gmail.com>
2023-01-11 20:53:58 +0100

    creating the login request

--- src/usermanagerapp/src/app/service/authentication.service.ts
+++ src/usermanagerapp/src/app/service/authentication.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment.development";
+import {HttpClient, HttpErrorResponse, HttpResponse} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../model/user";
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private host = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
+    return this.http
+      .post<HttpResponse<any> | HttpErrorResponse>(
+        `${this.host}/user/login`, user, {observe: 'response'}
+      );
+  }
 }
