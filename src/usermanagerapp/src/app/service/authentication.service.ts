cb403effae7c4238c2ac29ccdb305f39470ac50e
KENIS Mohamed Ali <kenis.mohamed.ali@gmail.com>
2023-01-11 21:09:00 +0100

    - creating the register & logout request - creating the ability to save the token to local storage

--- src/usermanagerapp/src/app/service/authentication.service.ts
+++ src/usermanagerapp/src/app/service/authentication.service.ts
@@ -11,6 +11,8 @@ import {User} from "../model/user";
 })
 export class AuthenticationService {
   private host = environment.apiUrl;
+  private token: string;
+  private loggedInUsername: string;
 
   constructor(private http: HttpClient) {
   }
@@ -21,4 +23,25 @@ export class AuthenticationService {
         `${this.host}/user/login`, user, {observe: 'response'}
       );
   }
+
+  public register(user: User): Observable<User | HttpErrorResponse> {
+    return this.http
+      .post<User | HttpErrorResponse>(
+        `${this.host}/user/register`, user
+      );
+  }
+
+  public logout(): void{
+   this.token = null;
+   this.loggedInUsername = null;
+   localStorage.removeItem('user');
+   localStorage.removeItem('token');
+    localStorage.removeItem('users');
+  }
+
+  public saveToken(token:string): void{
+    this.token = token;
+    localStorage.setItem('token', token);
+  }
+
 }
