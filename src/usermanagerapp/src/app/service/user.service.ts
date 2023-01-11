2a7336702434a6aa79e006c89b4dce5e4221d60c
KENIS Mohamed Ali <kenis.mohamed.ali@gmail.com>
2023-01-11 23:29:31 +0100

    add the ability to get all users in user.service.ts

--- src/usermanagerapp/src/app/service/user.service.ts
+++ src/usermanagerapp/src/app/service/user.service.ts
@@ -1,10 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment.development";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../model/user";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private host = environment.apiUrl;
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getUsers(): Observable<User[] | HttpErrorResponse> {
+    return this.http.get<User[]>(`${this.host}user/list`);
+  }
 
 
-  constructor() { }
+
 }
