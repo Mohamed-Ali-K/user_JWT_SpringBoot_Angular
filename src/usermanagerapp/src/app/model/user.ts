c1b8223ba3e594f43e91daf6b56332e9814bb583
KENIS Mohamed Ali <kenis.mohamed.ali@gmail.com>
2023-01-12 10:29:54 +0100

    adding properties to user.ts

--- src/usermanagerapp/src/app/model/user.ts
+++ src/usermanagerapp/src/app/model/user.ts
@@ -1,2 +1,32 @@
 export class User {
+  public userId: string;
+  public firstName: string;
+  public lastName: string;
+  public username: string;
+  public email: string;
+  public lastLoginDate: Date | null;
+  public lastLoginDateDisplay: Date | null;
+  public joinDate: Date | null;
+  public profileImageUrl: string;
+  public active: boolean;
+  public notLocked: boolean;
+  public role: string;
+  public authorities: [];
+
+  constructor() {
+    this.userId = '';
+    this.firstName = '';
+    this.lastName = '';
+    this.username = '';
+    this.email = '';
+    this.lastLoginDate = null;
+    this.lastLoginDateDisplay = null;
+    this.joinDate = null;
+    this.profileImageUrl = '';
+    this.active = false;
+    this.notLocked = false;
+    this.role = '';
+    this.authorities = [];
+  }
+
 }
