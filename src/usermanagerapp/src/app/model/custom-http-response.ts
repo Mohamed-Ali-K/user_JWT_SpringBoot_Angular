4acac0466866d23e55a54a600a2b04fb90572811
KENIS Mohamed Ali <kenis.mohamed.ali@gmail.com>
2023-01-12 11:18:01 +0100

    add documentation

--- src/usermanagerapp/src/app/model/custom-http-response.ts
+++ src/usermanagerapp/src/app/model/custom-http-response.ts
@@ -1,6 +1,22 @@
+/**
+ * The CustomHttpResponse interface represents a custom HTTP response object
+ * containing a status code, a status message, a reason and a message
+ */
 export interface CustomHttpResponse {
+  /**
+   * The HTTP status code of the response
+   */
   httpStatusCode: number;
+  /**
+   * The HTTP status message of the response
+   */
   httpStatus: string;
+  /**
+   * The reason for the response
+   */
   reason: string;
+  /**
+   * The message of the response
+   */
   message: string;
 }
--- src/usermanagerapp/src/app/model/user.ts
+++ src/usermanagerapp/src/app/model/user.ts
@@ -1,19 +1,71 @@
+/**
+ * The User class represents a user in the system.
+ * It contains properties for storing information about a user,
+ * such as their name, username, email, and role.
+ */
 export class User {
+  /**
+   * The id of the user
+   */
+  public id : number | null;
+  /**
+   * The user's unique identifier
+   */
   public userId: string;
+  /**
+   * The user's first name
+   */
   public firstName: string;
+  /**
+   * The user's last name
+   */
   public lastName: string;
+  /**
+   * The user's email address
+   */
   public username: string;
+  /**
+   * The user's email address
+   */
   public email: string;
+  /**
+   * The date of the user's last login
+   */
   public lastLoginDate: Date | null;
+  /**
+   * A string representation of the user's last login date for display purposes
+   */
   public lastLoginDateDisplay: Date | null;
+  /**
+   * The date the user joined the system
+   */
   public joinDate: Date | null;
+  /**
+   * The URL of the user's profile image
+   */
   public profileImageUrl: string;
+  /**
+   * The active status of the user
+   */
   public active: boolean;
+  /**
+   * The not locked status of the user
+   */
   public notLocked: boolean;
+  /**
+   * The role of the user
+   */
   public role: string;
+  /**
+   * The authorities of the user
+   */
   public authorities: [];
 
+  /**
+   * Constructs a new User instance
+   */
   constructor() {
+    this.id = null;
     this.userId = '';
     this.firstName = '';
     this.lastName = '';
