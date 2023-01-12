503509ef633def4629dc9f933bd5f17dd57e3902
KENIS Mohamed Ali <kenis.mohamed.ali@gmail.com>
2023-01-12 11:18:38 +0100

    - set the value of the observable to  the custom-http-response in resetPassword(), deleteUser() - changing the parameter type from string to number in user-service.ts#deleteUser() - add documentation

--- src/usermanagerapp/src/app/service/user.service.ts
+++ src/usermanagerapp/src/app/service/user.service.ts
@@ -3,28 +3,57 @@ import {environment} from "../../environments/environment.development";
 import {HttpClient, HttpErrorResponse, HttpEvent} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {User} from "../model/user";
+import {CustomHttpResponse} from "../model/custom-http-response";
 
+/**
+ * The UserService class is an Angular service that handles all user-related
+ * operations such as retrieving, adding, updating, and deleting users.
+ *
+ * @author Mohamed Ali Kenis
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private host = environment.apiUrl;
 
+  /**
+   * Constructs a new UserService instance with the given HTTP client.
+   *
+   * @param http the HTTP client used to make API calls
+   */
   constructor(private http: HttpClient) {
   }
 
+  /**
+   * Retrieves a list of all users.
+   *
+   * @return an observable of an array of users or an HTTP error response
+   */
   public getUsers(): Observable<User[] | HttpErrorResponse> {
     return this.http.get<User[]>(
       `${this.host}user/list`
     );
   }
 
+  /**
+   * Adds a new user.
+   *
+   * @param formData the form data containing the user information
+   * @return an observable of the added user or an HTTP error response
+   */
   public addUser(formData: FormData): Observable<User | HttpErrorResponse> {
     return this.http.post<User>(
       `${this.host}user/add`,
       formData);
   }
 
+  /**
+   * Updates an existing user.
+   *
+   * @param formData the form data containing the updated user information
+   * @return an observable of the updated user or an HTTP error response
+   */
   public updateUser(formData: FormData): Observable<User | HttpErrorResponse> {
     return this.http.post<User>(
       `${this.host}user/update`,
@@ -32,36 +61,67 @@ export class UserService {
     );
   }
 
-  public resetPassword(email: string): Observable<any| HttpErrorResponse> {
-    return this.http.get<User>(
+  /**
+   * Resets the password for a user with the given email address.
+   *
+   * @param email the email address of the user whose password should be reset
+   * @return an observable of a custom HTTP response or an HTTP error response
+   */
+  public resetPassword(email: string): Observable<CustomHttpResponse | HttpErrorResponse> {
+    return this.http.get<CustomHttpResponse>(
       `${this.host}user/reset-password/${email}`
     );
   }
 
-  public updateUserImage(formData: FormData):Observable<HttpEvent<User> | HttpErrorResponse> {
+  /**
+   * Update user profile image
+   *
+   * @param formData the form data containing the updated user information
+   * @return an observable of the updated user or an HTTP error response
+   */
+  public updateUserImage(formData: FormData): Observable<HttpEvent<User> | HttpErrorResponse> {
     return this.http.post<User>(
       `${this.host}user/updateProfileImage`,
       formData,
       {
-        reportProgress:true,
-        observe:'events'
+        reportProgress: true,
+        observe: 'events'
       });
   }
 
-  public deleteUser(identifier: string):Observable<any | HttpErrorResponse> {
-    return this.http.delete<any>(
-      `${this.host}user/delete/${identifier}`);
+  /**
+   * deletes user from the system based on the given identifier.
+   * @param id : unique identifier of the user.
+   * @return Observable<CustomHttpResponse | HttpErrorResponse> : returns observable of custom http response if successfull , or an error response if something went wrong.
+   */
+  public deleteUser(id: number): Observable<CustomHttpResponse | HttpErrorResponse> {
+    return this.http.delete<CustomHttpResponse>(
+      `${this.host}user/delete/${id}`);
   }
 
-  public addUsersToLocalCache(users:User[]):void{
-    localStorage.setItem('users',JSON.stringify(users) );
+  /**
+   * stores the users in the local storage.
+   * @param users : list of users to be stored.
+   */
+  public addUsersToLocalCache(users: User[]): void {
+    localStorage.setItem('users', JSON.stringify(users));
   }
 
-  public getUsersFromLocalCache():User[] | null{
+  /**
+   * retrieves users from local cache.
+   * @returns an array of User objects or null if not found.
+   */
+  public getUsersFromLocalCache(): User[] | null {
     const users = localStorage.getItem('users');
     return users ? JSON.parse(users) : null;
   }
-
+  /**
+   * creates a FormData object for adding/updating user.
+   * @param loggedInUserName the current user's username
+   * @param user an User object.
+   * @param profileImage a File object representing the user's profile image
+   * @returns a FormData object
+   */
   public createUserFormDate(loggedInUserName: string, user: User, profileImage: File): FormData {
     const formDate = new FormData();
     formDate.append('currentUsername', loggedInUserName);
