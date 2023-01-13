8b34ce1316bee89f462ac562e232fa5ea919b4d5
KENIS Mohamed Ali <kenis.mohamed.ali@gmail.com>
2023-01-13 17:23:18 +0100

    installing ngx-toastr

--- src/usermanagerapp/src/app/app.module.ts
+++ src/usermanagerapp/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {AuthInterceptor} from "./interceptor/auth.interceptor";
 import {AuthenticationGuard} from "./guard/authentication-guard.service";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ToastrModule} from "ngx-toastr";
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import {AuthenticationGuard} from "./guard/authentication-guard.service";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [
     AuthenticationGuard,
