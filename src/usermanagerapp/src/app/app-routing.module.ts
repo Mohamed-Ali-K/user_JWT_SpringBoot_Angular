b5c4d3a395851c8ff0095415eaf0d79cab2f3dbb
KENIS Mohamed Ali <kenis.mohamed.ali@gmail.com>
2023-01-15 22:18:05 +0100

    configuring the routes.

--- src/usermanagerapp/src/app/app-routing.module.ts
+++ src/usermanagerapp/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'/login',component: LoginComponent
+  },
+  {
+    path:'/register',component: RegisterComponent
+  },
+  {
+    path:'/user/management',component: LoginComponent
+  },
+  {
+    path:'/',redirectTo: '/login', pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
